19f3caa762d429b00507a3d412e54d27bfadc019
Rob Reinhold <rreinhold@expedia.com>
2016-02-10 11:23:20 -0600

    Added proxy goodness

--- rest-services/alice.js
+++ rest-services/alice.js
@@ -13,6 +13,15 @@ server.get('/', function (req, res, next) {
     return next();
 });
 
+// Call this to use the localhost polo-proxy router
+var client = restify.createJSONClient({url: 'http://localhost:5050'});
+server.get('/remote/:name', function (req, res, next) {
+    client.get("/" + req.params.name, function(cerr, creq, cres) {
+        res.send(cres.body);
+    });
+    return next();
+});
+
 server.listen(8080, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
