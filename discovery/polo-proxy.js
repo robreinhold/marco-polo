e897517c332a3bedb9a795ed1533bfaedea7d810
Rob Reinhold <rreinhold@expedia.com>
2016-02-15 09:42:05 -0600

    Set logging for the services and consul agent. polo-proxy picks healthy service at random.

--- discovery/polo-proxy.js
+++ discovery/polo-proxy.js
@@ -12,36 +12,45 @@ var consul = require('consul')({host: consul_url});
 var proxy = http_proxy.createProxyServer({ignorePath: true});
 
 var server = http.createServer(function(req, res) {
-    url_segs = req.url.split('/');
+    var url_segs = req.url.split('/');
     if(url_segs.length >= 2) {
-        service_name = url_segs[1];
+        var service_name = url_segs[1];
         consul.health.service(service_name, function(err, health_check_responses) {
             try {
+                var valid_urls = [];
                 for (var i = 0; i < health_check_responses.length; i++) {
-                    check_response = health_check_responses[i];
-                    service_id = check_response.Service.ID;
-                    checks = check_response.Checks;
+                    var check_response = health_check_responses[i];
+                    var service_id = check_response.Service.ID;
+                    var checks = check_response.Checks;
                     var dest_url = null;
                     for (var j = 0; j < checks.length; j++) {
-                        chk = checks[j];
+                        var chk = checks[j];
                         if (chk.CheckID == "service:" + service_id && chk.Status == "passing") {
-                            dest_url = "http://" + check_response.Node.Address + ":" + check_response.Service.Port;
+                            valid_urls.push("http://" + check_response.Node.Address + ":" + check_response.Service.Port);
                         }
                     }
                 }
-                if (dest_url != null) {
-                    proxy.web(req, res, {target: dest_url});
+                var num_urls = valid_urls.length;
+                if (num_urls > 0) {
+                    //Dumb load balancing - we have at least 1 healthy URL, pick one at random
+                    var url_index = Math.floor(Math.random() * num_urls);
+                    proxy.web(req, res, {target: valid_urls[url_index]});
                 } else {
                     res.writeHead(404);
                     res.end("polo-proxy: parsed '" + service_name + "' out of URL, but could not find healthy service with that name in consul.");
                 }
             } catch (ex) {
                 res.writeHead(500);
-                res.end(ex);
+                var vDebug = "";
+                for (var prop in ex)
+                {
+                    vDebug += "property: "+ prop+ " value: ["+ ex[prop]+ "]\n";
+                }
+                vDebug += "toString(): " + " value: [" + ex.toString() + "]";
+                res.end(vDebug);
             }
         });
     }
-
 });
 
 port = 5050;
--- rest-services/carles.js
+++ rest-services/carles.js
@@ -8,7 +8,13 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
+var logit = function(str) {
+    ts = new Date().toISOString();
+    console.log("%s - %s", ts, str);
+};
+
 server.get('/', function (req, res, next) {
+    logit('/');
     res.send("Hola, soy Carles");
     return next();
 });
@@ -16,6 +22,7 @@ server.get('/', function (req, res, next) {
 // Call this to use the localhost polo-proxy router
 var client = restify.createJSONClient({url: 'http://localhost:5050'});
 server.get('/remote/:name', function (req, res, next) {
+    logit(req.params.name);
     client.get("/" + req.params.name, function(cerr, creq, cres) {
         res.send(cres.body);
     });
